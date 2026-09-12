98898aad9405afa34d56d45c1c3d3484832b61c8
paulo talamini <paulo_talamini@hotmail.com>
2026-09-11 23:52:18 -0300

    AJUSTE MENUU 2

--- admin/js/components/menu.js
+++ admin/js/components/menu.js
@@ -1,10 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
-
 const container = document.querySelector(".container");
-
 const sidebar = document.querySelector(".sidebar");
 
-menuBtn.addEventListener("click",()=>{
+menuBtn.addEventListener("click", () => {
 
     if(window.innerWidth <= 768){
 
@@ -18,17 +16,24 @@ menuBtn.addEventListener("click",()=>{
 
 });
 
-document.addEventListener("click", function (evento) {
 
-    if (window.innerWidth > 768) return;
+/* =========================================
+   FECHAR SIDEBAR AO CLICAR FORA
+========================================= */
 
-    if (!sidebar.classList.contains("active")) return;
+document.addEventListener("click", function(evento){
+
+    if(window.innerWidth > 768) return;
+
+    if(!sidebar.classList.contains("active")) return;
 
     const clicouNaSidebar = sidebar.contains(evento.target);
-    const clicouNoMenu = menuBtn && menuBtn.contains(evento.target);
+    const clicouNoMenu = menuBtn.contains(evento.target);
 
-    if (!clicouNaSidebar && !clicouNoMenu) {
+    if(!clicouNaSidebar && !clicouNoMenu){
+
         sidebar.classList.remove("active");
+
     }
 
 });
